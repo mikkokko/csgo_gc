e4d1d7ffcc75fd51912ac42e1ed50bfa741165a8
drico <132640189+dricotec@users.noreply.github.com>
2026-04-22 16:19:49 +0400

    Revert Config.cpp & Confing .h changes?
    That was leftovers from another folder

--- csgo_gc/config.cpp
+++ csgo_gc/config.cpp
@@ -20,6 +20,8 @@ GCConfig::GCConfig()
         return;
     }
 
+    m_logOutput = config.GetNumber("log_output", m_logOutput);
+
     m_appIdOverride = config.GetNumber("appid_override", m_appIdOverride);
     m_showCsgoGCServersOnly = config.GetNumber("show_csgo_gc_servers_only", m_showCsgoGCServersOnly);
 
@@ -37,7 +39,6 @@ GCConfig::GCConfig()
     }
 
     m_destroyUsedItems = config.GetNumber("destroy_used_items", m_destroyUsedItems);
-    m_randomizeFloat = config.GetNumber("randomize_item_float", m_randomizeFloat);
 
     const KeyValue *rarityWeights = config.GetSubkey("rarity_weights");
     if (rarityWeights)
@@ -51,19 +52,6 @@ GCConfig::GCConfig()
             weight.rarity = FromString<uint32_t>(subkey.Name());
             weight.weight = FromString<float>(subkey.String());
             m_rarityWeights.push_back(weight);
-        }
-    }
-
-    const KeyValue *friends = config.GetSubkey("friends");
-    if (friends)
-    {
-        m_friends.clear();
-        m_friends.reserve(friends->SubkeyCount());
-
-        for (const KeyValue &subkey : *friends)
-        {
-            uint32_t friendId = FromString<uint32_t>(subkey.Name());
-            m_friends.push_back(friendId);
         }
     }
 
